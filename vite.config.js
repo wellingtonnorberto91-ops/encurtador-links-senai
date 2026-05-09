5b7bb22ee278528a5155958b35d77c31eeacfe42
wellingtonnorberto91-ops <wellington.norberto91@gmail.com>
2026-05-09 16:07:40 -0300

    Add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: resolve(__dirname, 'src/setupTests.js'),
+  },
+})
